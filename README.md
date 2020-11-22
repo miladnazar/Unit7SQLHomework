cabf6f21aa75066d75ee426818387f27155ff8ec
miladnazar <69940276+miladnazar@users.noreply.github.com>
2020-11-21 22:22:48 -0500

    Unit 7 Homework

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Unit7SQLHomework
+# Unit 7 Homework Assignment: Looking for Suspicious Transactions
+
+![Credit card fraudster](Images/Image_File_ERD.png)
+
+*[Credit Card Fraudster by Richard Patterson](https://www.flickr.com/photos/136770128@N07/42252105582/) | [Creative Commons Licensed](https://creativecommons.org/licenses/by/2.0/)*
+
+## Background
+
+In this Homework I will be investigating Fraud on different employees using their Identification Numbers provided by the employer. I will apply my new SQL skills to analyze historical credit card transactions and consumption patterns in order to identify possible fraudulent transactions.
+
+**This could be done through following the below three steps:**
+
+1. [Data Modeling](#Data-Modeling):
+2. [Data Engineering](#Data-Engineering):
+3. [Data Analysis](#Data-Analysis):
+
+---
+
+**My Submissions and Analyzations are explained in the below files:**
+
+* An image file of your ERD in DATA Folder.
+
+* A `.sql` file of your table schemata.
+
+* A Jupyter Notebook for the visual data analysis and the challenge.
+
+* A repository 
---
+++ table_schemata.sql
@@ -0,0 +1,66 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/OSPSGr
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "card_holder" (
+    "id" INT   NOT NULL,
+    "name" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_card_holder" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "credit_card" (
+    "card" VARCHAR(255)   NOT NULL,
+    "id_card_holder" INT   NOT NULL,
+    CONSTRAINT "pk_credit_card" PRIMARY KEY (
+        "card"
+     )
+);
+
+CREATE TABLE "merchant_category" (
+    "id" INT   NOT NULL,
+    "name" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_merchant_category" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "merchant" (
+    "id" INT   NOT NULL,
+    "name" VARCHAR(255)   NOT NULL,
+    "id_merchant_category" INT   NOT NULL,
+    CONSTRAINT "pk_merchant" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "transactions" (
+    "id" INT   NOT NULL,
+    "date" TIMESTAMP   NOT NULL,
+    "amount" FLOAT   NOT NULL,
+    "card" VARCHAR(255)   NOT NULL,
+    "id_merchant" INT   NOT NULL,
+    CONSTRAINT "pk_transactions" PRIMARY KEY (
+        "id"
+     )
+);
+
+ALTER TABLE "credit_card" ADD CONSTRAINT "fk_credit_card_id_card_holder" FOREIGN KEY("id_card_holder")
+REFERENCES "card_holder" ("id");
+
+ALTER TABLE "merchant" ADD CONSTRAINT "fk_merchant_id_merchant_category" FOREIGN KEY("id_merchant_category")
+REFERENCES "merchant_category" ("id");
+
+ALTER TABLE "transactions" ADD CONSTRAINT "fk_transactions_card" FOREIGN KEY("card")
+REFERENCES "credit_card" ("card");
+
+ALTER TABLE "transactions" ADD CONSTRAINT "fk_transactions_id_merchant" FOREIGN KEY("id_merchant")
+REFERENCES "merchant" ("id");
+
+SELECT * FROM card_holder
+SELECT * FROM credit_card
+SELECT * FROM merchant
+SELECT * FROM merchant_category
+SELECT * FROM transactions
